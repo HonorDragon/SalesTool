79ec6a6d25a307342f8a8634273fbeea89de9df2
HonorDragon <hidronefelit@gmail.com>
2014-03-21 21:05:45 +0100

    Initial checkin of sales tool.

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,21 @@
+package ca.demo.salestool;
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+		SalesData data = new SalesData();
+		displayGreeting();
+		data.display();
+	
+	}
+	
+	private static void displayGreeting()
+	{
+		System.out.println("First message");
+		System.out.println("Second message");
+		
+	}
+	
+	
+}
